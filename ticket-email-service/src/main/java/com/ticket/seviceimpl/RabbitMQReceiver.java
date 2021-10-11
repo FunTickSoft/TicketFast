d7b856b657f2dc56fd48db85628b060e511c7afe
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-10-11 03:42:33 +0300

    change mail service

--- ticket-email-service/src/main/java/com/ticket/seviceimpl/RabbitMQReceiver.java
+++ ticket-email-service/src/main/java/com/ticket/seviceimpl/RabbitMQReceiver.java
@@ -6,6 +6,7 @@ import com.example.email.model.MessageMail;
 import com.ticket.config.RabbitConfigurationProperties;
 import com.ticket.service.MailSendingService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -27,10 +28,9 @@ public class RabbitMQReceiver {
     }
 
 
-    @RabbitListener(bindings = @QueueBinding(value = @Queue("#{rabbitConfigurationProperties.getNameQueue()}"),
-    exchange = @Exchange(value = "#{rabbitConfigurationProperties.getNameExchange}"),
-    key = "#{rabbitConfigurationProperties.getNameRoutingKey()}"))
+    @RabbitListener(queues = "#{rabbitConfigurationProperties.getNameQueue()}")
     public void onMessage(MessageMail messageModel, Message message) {
+
         log.info("Get Message from Queue {}, key {}", messageModel, message.getMessageProperties().getReceivedRoutingKey() );
         service.send(messageModel);
         log.info("send email");
